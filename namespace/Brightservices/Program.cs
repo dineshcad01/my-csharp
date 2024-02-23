ac961754c3f3f6296847371d356e5c146a16c081
dineshcad01 <tdineshcad01@gmail.com>
2024-02-23 17:37:32 +0530

    namespace folder added

---
+++ namespace/Brightservices/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace brightservices{
+
+    class Program{
+        static void Main(string[] args) {
+
+           bright bt = new bright();
+
+            Console.WriteLine("Brightservices family");
+            Console.WriteLine("I have " + bt.getinstruments());
+            Console.WriteLine("I have " + bt.getfreeclasses());
+
+
+         
+        }
+    }
+}
---
+++ namespace/Brightservices/bright.cs
@@ -0,0 +1,34 @@
+namespace brightservices{
+
+class bright{
+    string instruments, freeclasses, workers;
+    public bright() {
+        instruments="Computers";
+        freeclasses="Could Application Devoleper free classes ";
+        workers="AR callers";
+}
+
+     public string getinstruments(){
+        return instruments;
+     }
+
+     public string getfreeclasses(){
+        return freeclasses;
+     }
+
+     public string getworkers(){
+        return workers;
+     }
+
+
+
+
+}
+
+
+
+
+
+
+}
+
